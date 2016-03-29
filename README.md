22f5e7c7682d651a80c757d7610b4d71427ae333
Sam Wilson <sam@radius.com>
2016-03-29 10:12:56 -0500

    made it robust for animation

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # scratchpad-vanilla
-My starting point for a new project or quick proof of concept. Evolves over time.
+My starting point for a new animation quick proof of concept. Evolves over time.
 
 # Installation
-* Fork this repo
 * Clone _your_ version into some directory on your machine
+* optionally: run ```rm -rf .git``` iff you want to dissociate from this repo
+* run ```bower install``` in that directory
 * run ```npm install``` in that directory
 * run ```gulp```
 
+# Always branch
+To start a new experiment, just do
+```git checkout -b some-animation-branch```
+
+Do your experiments and post your branches, when you are ready for another experiment just start another branch.
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,25 @@
-// Your .js
+// Requirements
+
+var TweenLite = require('../../bower_components/gsap/src/uncompressed/TweenLite.js');
+var TweenMax = require('../../bower_components/gsap/src/uncompressed/TweenMax.js');
+var TimelineLite = require('../../bower_components/gsap/src/uncompressed/TimelineLite.js');
+var TimelineMax = require('../../bower_components/gsap/src/uncompressed/TimelineMax.js');
+var CSSPlugin = require('../../bower_components/gsap/src/uncompressed/plugins/CSSPlugin.js');
+
+// Animating code
+// see http://greensock.com/forums/topic/8370-gsap-demos-snippets-pens/
+// see https://greensock.com/jump-start-js#basic-tween
+// see https://ihatetomatoes.net/simple-greensock-tutorial-your-first-steps-with-gsap/
+//
+// var element = $('.obj');
+//
+// var tl = new TimelineMax({repeat:2, repeatDelay:1});
+// tl.add( TweenLite.to(element, 1, {left:100}) );
+// tl.add( TweenLite.to(element, 1, {top:50}) );
+// tl.add( TweenLite.to(element, 1, {opacity:0}) );
+//
+// //then later, control the whole thing...
+// tl.play();
+
+var element = $('#hi');
+TweenLite.to(hi, 1, {opacity: .25, scale: 3});

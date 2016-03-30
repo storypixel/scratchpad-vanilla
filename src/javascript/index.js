e2e393e1e1c9a8f3c222b4d03ff0bf2f16e1fc60
Sam Wilson <sam@radius.com>
2016-03-30 12:26:42 -0500

    saved this branch from destruction

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -21,5 +21,30 @@ var CSSPlugin = require('../../bower_components/gsap/src/uncompressed/plugins/CS
 // //then later, control the whole thing...
 // tl.play();
 
-var element = $('#hi');
-TweenLite.to(hi, 1, {opacity: .25, scale: 3});
+
+// Animating code
+// see http://greensock.com/forums/topic/8370-gsap-demos-snippets-pens/
+// see https://greensock.com/jump-start-js#basic-tween
+// see https://ihatetomatoes.net/simple-greensock-tutorial-your-first-steps-with-gsap/
+//
+// var element = $('.obj');
+//
+// var tl = new TimelineMax({repeat:2, repeatDelay:1});
+// tl.add( TweenLite.to(element, 1, {left:100}) );
+// tl.add( TweenLite.to(element, 1, {top:50}) );
+// tl.add( TweenLite.to(element, 1, {opacity:0}) );
+//
+// //then later, control the whole thing...
+// tl.play();
+
+// [left, top] positions
+var dpos = [[0, 0], [25, 0], [50, 0], [75, 0], [100, 0], [100, 25], [100, 50], [100, 75], [100, 100], [75, 100], [50, 100], [25, 100], [0, 100], [0, 75], [0, 50], [0, 25]];
+
+TweenLite.to($('.dancer'), 20, { rotation: 360 * 2 });
+
+dpos.forEach(function (el, i) {
+  var leftv = el[0] + '%';
+  var topv = el[1] + '%';
+  var kolor = "hsl(" + i * 20 % 360 + ", 100%, 50%)";
+  TweenLite.to($('.dancer--' + (i % dpos.length + 1)), 1, { delay: i * .25, opacity: 1, width: '1vw', height: '1vw', left: leftv, top: topv, 'background-color': kolor, 'border-radius': '50%' });
+});
